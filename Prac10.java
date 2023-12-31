752e949b3a6d0c64030cf4b30d17ca8f39a2e111
Arka <146548597+Arkadeep18@users.noreply.github.com>
2023-12-31 23:06:02 +0530

    Add files via upload

---
+++ Prac10.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Prac10 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a Number");
+		int n = sc.nextInt();
+		
+		for (int a = 1; a <= 10; a++ ) {
+			System.out.println(a*n);
+		}
+
+	}
+
+}
